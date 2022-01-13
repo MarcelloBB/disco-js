a87a6e9f66a72603a8bea8f19985b8feb309de9e
MarcelloBB <marcellobelanda@gmail.com>
2022-01-13 17:45:09 -0300

    Add c[add]

---
+++ main.js
@@ -0,0 +1,23 @@
+// Import packages
+const program = require("commander");
+const package = require("./package.json");
+
+// Add version
+program.version(package.version);
+
+// Add first CLI program
+//          * add [todo]
+//          We use command() and then we give a string and the parameters
+// [] = optional
+// <> = required
+//          We use description() to give --help flag text
+
+program
+    .command("add [todo]")
+    .description("Adiciona uma tarefa")
+    .action((todo) => {
+        console.log(todo);
+    });
+
+// Node need this to understand commands
+program.parse(process.argv);
