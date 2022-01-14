d379d9f9f2d4d9a638f09b8f793b17b69e4ac2d7
MarcelloBB <marcellobelanda@gmail.com>
2022-01-14 13:50:40 -0300

    Created module.exports structure

--- functions.js
+++ functions.js
@@ -1,9 +1,22 @@
 /* =========================================================*/
-const {  exp, pow, sqrt, random, round  } = Math;
 
-const genRandomInt = (coeficient = 10) => {
-    const RANDOM_INT = round(random() * coeficient);
-    return RANDOM_INT;
+module.exports = {
+    // *******************************************************
+    // [Math]  functions
+    // *******************************************************
+    round:  Math.round(),
+    exp:    Math.exp(),
+    pow:    Math.pow(),
+    sqrt:   Math.sqrt(),
+    random: Math.random(),
+
+    // *******************************************************  
+    // Other  functions
+    // *******************************************************
+    genRandomInt: function (coeficient = 10) {
+        const RANDOM_INT = round(random() * coeficient);
+        return RANDOM_INT;
+    }
 }
 /* =========================================================*/
 
