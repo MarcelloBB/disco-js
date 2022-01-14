61f762014d9f116d5d689e727543b3803aea3aa6
MarcelloBB <marcellobelanda@gmail.com>
2022-01-14 14:21:52 -0300

    Finish sumAction

---
+++ actions.js
@@ -0,0 +1,19 @@
+const sumAction = (a, b) => {
+    console.log(``);
+    console.log(`╔═══════════════════════════╗`);
+    console.log(`║         Disco-js          ║`);
+    console.log(`╚════════╦════════╦═════════╝`);
+    console.log(`         ║  v1.0  ║          `);
+    console.log(`         ╚════════╝`);
+    console.log(``);
+    console.log(` ▓ OPERATION:`);
+    console.log(`   [+] SUM`);
+    console.log(``);
+    console.log(` ▓ RESULTS:`);
+    console.log(`   ${a} + ${b} = ${parseInt(a) + parseInt(b)}`);
+    console.log(``);
+}
+
+module.exports = {
+    sumAction: sumAction
+}
